1aa6087375056b0ca63195cc2c0def705c086e00
wanlab-io <hello.wanlab@gmail.com>
2020-05-03 23:33:18 +0800

    add fetch repos

--- github.js
+++ github.js
@@ -3,6 +3,11 @@ class Github {
   constructor() {
     this.client_id = 'e8a4aedf7de85914c316';
     this.client_secret = '2f6e108312b454c057f6a8252f2461819ce74244'; 
+
+    // set repos count display
+    this.repos_count = 5;
+    // set repos sort by lates
+    this.repos_sort = 'created: asc';
   }
 
   // GET request
@@ -10,12 +15,17 @@ class Github {
     // fetch user data from api
     const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    // fetch user repos
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     // set json data to variable
     const profile = await profileResponse.json();
+    const repos = await repoResponses.json();
 
     // return data
     return {
-      profile
+      profile,
+      repos
     }
 
   }
